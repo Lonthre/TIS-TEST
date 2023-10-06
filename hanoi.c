c8942b12a491e1fc0eacf69bd7bbb57bd48e8da3
Marianna811 <125303993+Marianna811@users.noreply.github.com>
2023-10-06 14:52:05 +0200

    Update hanoi.c
    opravené chyby

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("presuò disk 1 z %d na %d\n", a, b);
+    printf("presun disk 1 z %d na %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a - b);
-    printf("presuò disk %d na %d z %d\n", n, a, b);
+    printf("presun disk %d z %d na %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
